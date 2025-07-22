217fd590753b290e819c87c30eb60fd15871694b
dmitrii_mishchenko <dmittry.m@gmail.com>
2025-07-22 16:06:17 +0300

    BASE_APP: migration

---
+++ app/migrations/versions/49bd1b68269f_.py
@@ -0,0 +1,50 @@
+"""empty message
+
+Revision ID: 49bd1b68269f
+Revises:
+Create Date: 2025-07-22 07:43:28.592647
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "49bd1b68269f"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "achievements",
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.Column("name", sa.String(length=32), nullable=False),
+        sa.Column("unlocked_at", sa.DateTime(), nullable=False),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("user_id", "name", name="uix_achievements_user_name"),
+    )
+    op.create_index(op.f("ix_achievements_id"), "achievements", ["id"], unique=False)
+    op.create_table(
+        "events",
+        sa.Column("user_id", sa.Integer(), nullable=False, comment="id пользователя."),
+        sa.Column(
+            "event_type", sa.String(length=32), nullable=False, comment="Тип события"
+        ),
+        sa.Column("details", sa.JSON(), nullable=True),
+        sa.Column("created_at", sa.DateTime(), nullable=False),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_index(op.f("ix_events_id"), "events", ["id"], unique=False)
+
+
+def downgrade() -> None:
+    op.drop_index(op.f("ix_events_id"), table_name="events")
+    op.drop_table("events")
+    op.drop_index(op.f("ix_achievements_id"), table_name="achievements")
+    op.drop_table("achievements")
